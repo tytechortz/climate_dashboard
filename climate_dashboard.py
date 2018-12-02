570b5e460e8282a8f87319f5a1cd40bae706f0b3
James <tytechortz@gmail.com>
2018-12-02 16:57:11 -0700

    adds Boulder and Utqiagvik data

--- climate_dashboard.py
+++ climate_dashboard.py
@@ -6,4 +6,28 @@ import plotly.graph_objs as go
 import pandas as pd
 
 df = pd.read_csv('./data/Stapleton.csv')
-print(df.head())
+
+
+app = dash.Dash()
+
+year_options = []
+for year in df['YEAR'].unique():
+    year_options.append({'lable':str(year),'value':year})
+
+app.layout = html.Div([
+    dcc.Graph(id='graph'),
+    dcc.Dropdown(id='year-picker', options=year_options)
+                value=df['YEAR'].min())
+])
+
+@app.callback(Output('graph', 'figure'),
+            [Input('year-picker', 'value')])
+def update_figure(selected_year):
+    return #something
+
+    filtered_df = df[df['YEAR']== selected_year]
+
+    traces 
+
+if __name__ == '__main__':
+    ap.run.server()

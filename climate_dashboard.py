c39af5afdfb972121a1c25eeacb20d7d1252f766
James <tytechortz@gmail.com>
2018-12-02 16:08:31 -0700

    prints head of data csv

---
+++ climate_dashboard.py
@@ -0,0 +1,9 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('./data/Stapleton.csv')
+print(df.head())

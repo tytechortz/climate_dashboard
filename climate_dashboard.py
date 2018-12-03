97835126c23c63172bcf99cc90548e868134581d
James <tytechortz@gmail.com>
2018-12-03 08:47:23 -0700

    starts map, continues first chart

--- climate_dashboard.py
+++ climate_dashboard.py
@@ -25,6 +25,19 @@ app.layout = html.Div([
     dcc.Dropdown(id='month-picker',options=month_options,value=df['month'].min())
 ])
 
+@app.callback(Output('graph', 'figure'),
+        [Input('month-picker', 'value')])
+def update_figure(selected_month):
+    filtered_df = df[df['month'] == selected_month]
+        traces = []
+        for station_name in filtered_df['station'] == station_name]
+        traces.append(go.Scatter(
+            x=df_by_continent[''],
+            y=df_by_continent[''],
+            text=df_by_continent[''],
+            mode='markers',
+            opacity=0.7,
+        ))
 
 
 if __name__ == '__main__':

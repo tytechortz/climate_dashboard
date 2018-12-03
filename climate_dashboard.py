04c21a4d0f9271991d07d819f27dad9b8c9f511b
James <tytechortz@gmail.com>
2018-12-02 20:22:03 -0700

    adds station dropdown selector

--- climate_dashboard.py
+++ climate_dashboard.py
@@ -10,11 +10,17 @@ df = pd.read_csv('./data/Boulder_USCRN.csv')
 
 app = dash.Dash()
 
+station_options = []
+for station in df['station'].unique():
+    station_options.append({'label':str(station), 'value':station})
+
+
 month_options = []
 for month in df['month'].unique():
     month_options.append({'label':str(month), 'value':month})
 
 app.layout = html.Div([
+    dcc.Dropdown(id='station-picker',options=station_options,value=df['station']),
     dcc.Graph(id='graph'),
     dcc.Dropdown(id='month-picker',options=month_options,value=df['month'].min())
 ])

d1aee569431a29028ad6f9187c8963b87a4e974e
James <tytechortz@gmail.com>
2018-12-03 22:14:24 -0700

    adds graph for monthly min temps

--- climate_dashboard.py
+++ climate_dashboard.py
@@ -17,15 +17,31 @@ for station in df['station'].unique():
 
 
 app.layout = html.Div([
-    dcc.Graph(id='graph'),
-    dcc.Dropdown(id='station-picker',options=station_options,value=df['station']),
+
+    html.Div([
+        dcc.Graph(id='MAX_graph'),
+        html.Label('Dropdown'),
+        dcc.Dropdown(
+            id='station-picker_max',
+            options=station_options,value=df['station'],
+            placeholder = "Select Station",
+        ),
+    ]),
+
+    html.Div([
+        dcc.Graph(id='MIN_graph'),
+        html.Label('Dropdown'),
+        dcc.Dropdown(
+            id='station-picker_min',
+            options=station_options,value=df['station'],
+            placeholder = "Select Station",
+        ),
+    ])
 ])
 
 
-
-
-@app.callback(Output('graph', 'figure'),
-        [Input('station-picker', 'value')])
+@app.callback(Output('MAX_graph', 'figure'),
+        [Input('station-picker_max', 'value')])
 def update_figure(selected_station):
     filtered_df = df[df['station'] == selected_station]
     traces = []
@@ -44,12 +60,38 @@ def update_figure(selected_station):
     return {
         'data': traces,
         'layout': go.Layout(
-            xaxis={'title': 'Month'},
-            yaxis={'title': 'Temp'},
+            xaxis={'title': 'Running Total Months'},
+            yaxis={'title': 'Temp in Deg C'},
             hovermode='closest'
         )
     }
 
+
+@app.callback(Output('MIN_graph', 'figure'),
+        [Input('station-picker_min', 'value')])
+def update_figure(selected_station):
+    filtered_df = df[df['station'] == selected_station]
+    traces = []
+    for station_name in filtered_df['station'].unique():
+        df_by_station = filtered_df[filtered_df['station'] == station_name]
+        traces.append(go.Scatter(
+            x=df_by_station['RUN_MON'],
+            y=df_by_station['T_MIN'],
+            text=df_by_station['station'],
+            mode='markers+lines',
+            opacity=0.7,
+            marker={'size': 5},
+            name=station_name
+        ))
+
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            xaxis={'title': 'Running Total Months'},
+            yaxis={'title': 'Temp in Deg C'},
+            hovermode='closest'
+        )
+    }
 
 if __name__ == '__main__':
     app.run_server()
